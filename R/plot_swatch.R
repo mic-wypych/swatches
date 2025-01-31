03726ea94519ac90f04e66f94bdea6ce4b3fd4bf
MWypych <wypych.mic@gmail.com>
2025-01-31 11:25:04 +0100

    init swatches
    start a package to plot color swatches and color wheel in ggplot2

---
+++ R/plot_swatch.R
@@ -0,0 +1,40 @@
+plot_swatch <- function(palette, nrow = 1) {
+  require(ggplot2)
+  require(dplyr)
+  require(tidyr)
+  require(forcats)
+  require(ggfittext)
+  require(glue)
+  require(colorspace)
+
+  stopifnot(is.vector(palette))
+
+  palette_df <- data.frame(hex = palette) |>
+    mutate(id = row_number()) |>
+    bind_cols(as.data.frame(colorspace::hex2RGB(palette)@coords))
+
+  palette_df <- palette_df |>
+    bind_cols(
+      rgb2hsv(palette_df$R, palette_df$G, palette_df$B) |>
+        t() |>
+        as.data.frame()
+    )  %>%
+    mutate(across(.cols = R:B, ~ . * 255),
+           across(.cols = h:v, \(x) round(x, 3)))
+
+  palette_df$hex <- fct_inorder(palette_df$hex)
+  palette_df |>
+    ggplot(aes(xmin=0, xmax = 1, ymin = 0, ymax = 1, fill = hex)) +
+    geom_rect() +
+    geom_fit_text(aes(x = .5, y = .7, fill = hex, label = hex), contrast = TRUE, inherit.aes = FALSE, fontface = "bold") +
+    geom_fit_text(aes(x = .5, y = .5,fill = hex, label = glue::glue("R: {palette_df$R} G: {palette_df$G} B: {palette_df$B}")), contrast = T, inherit.aes = F, fontface = "bold") +
+    geom_fit_text(aes(x = .5, y = .3,fill = hex, label = glue::glue("H: {palette_df$h} S: {palette_df$s} V: {palette_df$v}")), contrast = T, inherit.aes = F, fontface = "bold") +
+    scale_fill_identity() +
+    facet_wrap(~hex, nrow = nrow) +
+    coord_fixed() +
+    theme_void() +
+    theme(legend.position = "none",
+          plot.background = element_rect(fill = NA, color = NA),
+          strip.text = element_blank())
+}
+
---
+++ R/plot_wheel.R
@@ -0,0 +1,47 @@
+
+# color wheel
+
+
+plot_wheel <- function(palette) {
+  require(ggplot2)
+  require(dplyr)
+  require(tidyr)
+  require(colorspace)
+
+  palette_df <- data.frame(hex = palette) |>
+    mutate(id = row_number()) |>
+    bind_cols(
+      as.data.frame(colorspace::hex2RGB(palette)@coords)) # Normalize to [0,1]
+
+  palette_df <- palette_df |>
+    bind_cols(
+      rgb2hsv(palette_df$R, palette_df$G, palette_df$B) |>
+        t() |>
+        as.data.frame()
+    )
+
+
+  hue_values <- seq(0, 1, length.out = 360)  # 360 hue values
+  color_data <- data.frame(
+    xmin = hue_values,
+    xmax = hue_values + (1/360),  # Small increment to create adjacent rectangles
+    ymin = 1.1,  # Bottom of the rectangle
+    ymax = 1.2,  # Top of the rectangle
+    color = hsv(hue_values, 1, 1)  # Convert hue to colors
+  )
+
+
+
+  palette_df %>%
+    ggplot(aes(x = h, y = s, color = hex)) +
+    geom_point(size = 7) +
+    geom_rect(data = color_data, aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax, fill = color), inherit.aes = FALSE) +
+    scale_fill_identity() +
+    scale_color_identity() +
+    scale_x_continuous(breaks = c(0,.1,.2,.3,.4,.5,.6,.7,.8,.9), expand = c(0,0)) +
+    scale_y_continuous(limits = c(0, 1.2), expand = c(0,-.15)) +
+    coord_polar(clip = "off") +
+    theme_void() +
+    theme(legend.position = "none",
+          panel.grid.major = element_line(color = "grey80"))
+}
